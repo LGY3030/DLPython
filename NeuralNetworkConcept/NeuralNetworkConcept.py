aa89285526aa2b95288f0cfda2ca4c6e4c24efbc
GuanYu Lin <lgy3030@gmail.com>
2018-10-30 21:10:22 +0800

    Update
    Practice NeuralNetworkConcept

--- NeuralNetworkConcept/NeuralNetworkConcept.py
+++ NeuralNetworkConcept/NeuralNetworkConcept.py
@@ -146,3 +146,84 @@ print(x.shape)
 print(y.shape)
 print(np.dot(x,w))
 
+
+# In[17]:
+
+
+# Neural Network ---> four layers --- one input(two nodes),two middle(three nodes and two nodes),and one output(two nodes)
+
+import numpy as np
+
+def sigmoid_function(x):
+    return 1/(1+np.exp(-x))
+
+def lastlayer_function(x):
+    return x
+
+# input to 1st middle
+X=np.array([1.0,0.5])
+W1=np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+B1=np.array([0.1,0.2,0.3])
+A1=np.dot(X,W1)+B1
+Z1=sigmoid_function(A1)
+
+# 1st middle to 2st middle
+W2=np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+B2=np.array([0.1,0.2])
+A2=np.dot(Z1,W2)+B2
+Z2=sigmoid_function(A2)
+
+# 2st middle to output
+W3=np.array([[0.1,0.3],[0.2,0.4]])
+B3=np.array([0.1,0.2])
+A3=np.dot(Z2,W3)+B3
+Y=lastlayer_function(A3)
+
+print(Y)
+
+
+# In[18]:
+
+
+# Neural Network ---> four layers --- one input(two nodes),two middle(three nodes and two nodes),and one output(two nodes)
+# (another)
+# only W use capital letter
+
+import numpy as np
+
+def sigmoid_function(x):
+    return 1/(1+np.exp(-x))
+
+def lastlayer_function(x):
+    return x
+    
+def init_network():
+    network={}
+    network['W1']=np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+    network['b1']=np.array([0.1,0.2,0.3])
+    network['W2']=np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+    network['b2']=np.array([0.1,0.2])
+    network['W3']=np.array([[0.1,0.3],[0.2,0.4]])
+    network['b3']=np.array([0.1,0.2])
+    
+    return network
+
+def forward(network,x):
+    W1,W2,W3=network['W1'],network['W2'],network['W3']
+    b1,b2,b3=network['b1'],network['b2'],network['b3']
+    
+    a1=np.dot(x,W1)+b1
+    z1=sigmoid_function(a1)
+    a2=np.dot(z1,W2)+b2
+    z2=sigmoid_function(a2)
+    a3=np.dot(z2,W3)+b3
+    y=lastlayer_function(a3)
+    
+    return y
+
+
+network=init_network()
+x=np.array([1.0,0.5])
+y=forward(network,x)
+print(y)
+
