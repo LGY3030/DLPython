5a1733e5c12d0973050958234ea33993b1bbd16e
GuanYu Lin <lgy3030@gmail.com>
2018-11-01 21:48:15 +0800

    Update
    Practice NeuralNetworkConcept

--- NeuralNetworkConcept/NeuralNetworkConcept.py
+++ NeuralNetworkConcept/NeuralNetworkConcept.py
@@ -322,7 +322,7 @@ print(img.shape)
 img_show(img)
 
 
-# In[19]:
+# In[31]:
 
 
 # test data with sample_weight.pkl which is trained already
@@ -366,5 +366,75 @@ for i in range(len(x)):
     p=np.argmax(y)
     if p==t[i]:
         accuracy_cnt += 1
+print("Accuracy:" + str(float(accuracy_cnt)/len(x)))
+
+
+# In[33]:
+
+
+# Neural Network ---> 784 and (784 x 50) and (50 x 100) and (100 x 10) and 10
+
+x,t=get_data()
+network=init_network()
+W1,W2,W3=network['W1'],network['W2'],network['W3']
+b1,b2,b3=network['b1'],network['b2'],network['b3']
+print(x.shape)
+print(x[0].shape)
+print(W1.shape)
+print(b1.shape)
+print(W2.shape)
+print(b2.shape)
+print(W3.shape)
+print(b3.shape)
+
+
+# In[41]:
+
+
+# batch
+# Neural Network ---> 100 x 784 and (784 x 50) and (50 x 100) and (100 x 10) and 100 x 10
+
+import numpy as np
+from dataset.mnist import load_mnist
+import pickle
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+def softmax(a):
+    c=np.max(a)
+    exp_a=np.exp(a-c)
+    sum_exp_a=np.sum(exp_a)
+    y=exp_a/sum_exp_a
+    return y
+def get_data():
+    (x_train,t_train),(x_test,t_test)=load_mnist(flatten=True,normalize=True,one_hot_label=False)
+    return x_test,t_test
+
+def init_network():
+    with open("sample_weight.pkl",'rb') as f:
+        network=pickle.load(f)
+    return network
+def predict(network,x):
+    W1,W2,W3=network['W1'],network['W2'],network['W3']
+    b1,b2,b3=network['b1'],network['b2'],network['b3']
+
+    a1=np.dot(x,W1)+b1
+    z1=sigmoid(a1)
+    a2=np.dot(z1,W2)+b2
+    z2=sigmoid(a2)
+    a3=np.dot(z2,W3)+b3
+    y=softmax(a3)
+    return y
+
+x,t=get_data()
+network=init_network()
+batch_size=100
+accuracy_cnt=0
+for i in range(0,len(x),batch_size):
+    x_batch=x[i:i+batch_size]
+    y_batch=predict(network,x_batch)
+    # axis bases on 1-D or 2-D (axis=0:1-D,axis=1:2-D)
+    p=np.argmax(y_batch,axis=1)
+    accuracy_cnt+=np.sum(p==t[i:i+batch_size])
 print("Accuracy:" + str(float(accuracy_cnt)/len(x)))
 
