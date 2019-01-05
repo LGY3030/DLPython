94fb891371d8998a97e9efe284d15dea2fceb7f5
GuanYu Lin <lgy3030@gmail.com>
2019-01-05 23:02:04 +0800

    Update

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -1,72 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[8]:
 
 
-# import package
 from datetime import date,timedelta
 from urllib.request import urlopen
 from dateutil import rrule
-import matplotlib.pyplot as plt
 import datetime
 import pandas as pd
 import numpy as np
 import json
 import time
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+
+# In[9]:
 
 
-# In[2]:
-
-
-# 爬取每月股價的目標網站並包裝成函式
-def craw_one_month(stock_number,date):
-    url = (
-        "http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date="+
-        date.strftime('%Y%m%d')+
-        "&stockNo="+
-        str(stock_number)
-    )
+# 抓取每個月的資料
+def getone(stock,month):
+    url=("http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date="+month.strftime('%Y%m%d')+"&stockNo="+str(stock))
     data = json.loads(urlopen(url).read())
     return pd.DataFrame(data['data'],columns=data['fields'])
 
 
-# In[78]:
+# In[12]:
 
 
-# 根據使用者輸入的日期，以月為單位，重複呼叫爬取月股價的函式
-def craw_stock(stock_number, start_month):
-    b_month = date(*[int(x) for x in start_month.split('-')])
-    now = "2018-11-25"#datetime.datetime.now().strftime("%Y-%m-%d")         # 取得現在時間
-    e_month = date(*[int(x) for x in now.split('-')])
+# 抓取全部時間的資料 (利用getone function)
+def getdata(stock,start,end):
+    a=[0,0,0]
+    i=0
+    for x in start.split('-'):
+        a[i]=int(x)
+        i=i+1
+    begin = date(a[0],a[1],a[2])
+    b=[0,0,0]
+    j=0
+    for x in end.split('-'):
+        b[j]=int(x)
+        j=j+1
+    final = date(b[0],b[1],b[2])
     
-    result = pd.DataFrame()
-    for dt in rrule.rrule(rrule.MONTHLY, dtstart=b_month, until=e_month):
-        result = pd.concat([result,craw_one_month(stock_number,dt)],ignore_index=True)
-        time.sleep(5000.0/1000.0);
+    data = pd.DataFrame()
     
-    return result
-
-df = craw_stock(2330,"2018-01-01")
-df.set_index("日期", inplace=True)
-
-
-# In[79]:
+    for month in rrule.rrule(rrule.MONTHLY, dtstart=begin, until=final):
+        data = pd.concat([data,getone(stock,month)],ignore_index=True)
+        time.sleep(9000.0/1000.0);
+    
+    return data
 
 
-# 將爬取到的歷年股價資訊繪成圖表
-df['收盤價']=df['收盤價'].astype(float)
-df.loc[:]['收盤價'].plot(figsize=(18, 8))
-plt.xlabel('month')
-plt.ylabel('stock')
+# In[13]:
 
 
-# In[80]:
-
-
-df.to_csv('2018.csv', encoding='utf_8_sig')
+crawl = getdata(2302,"2012-01-01","2012-12-31")
+crawl.set_index("日期", inplace=True)
+crawl.to_csv('2012.csv', encoding='utf_8_sig')
 
 
 # In[ ]:
