00b9139cb3677bca905b4f24d783efa09143b05a
GuanYu Lin <lgy3030@gmail.com>
2019-01-09 00:14:28 +0800

    Update

---
+++ test/test.py
@@ -0,0 +1,262 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+# RNN(Long Short-Term Memory, LSTM)
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, TimeDistributed, RepeatVector
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+# 讀取資料
+def readTrain():
+  train = pd.read_csv("S&P 500.csv")
+  return train
+
+
+# In[4]:
+
+
+# Augment Features
+def augFeatures(train):
+  train["Date"] = pd.to_datetime(train["Date"])
+  train["year"] = train["Date"].dt.year
+  train["month"] = train["Date"].dt.month
+  train["date"] = train["Date"].dt.day
+  train["day"] = train["Date"].dt.dayofweek
+  return train
+
+
+# In[5]:
+
+
+# Normalization
+def normalize(train):
+  train = train.drop(["Date"], axis=1)
+  train_norm = train.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
+  print(np.mean(train))
+  return train_norm
+
+
+# In[6]:
+
+
+# Build Training Data
+# X_train: 利用前30天的Open, High, Low, Close, Adj Close, Volume, month, year, date, day作為Features
+# Y_train: 利用未來5天的Adj Close作為Features
+# 須將資料做位移的展開作為Training Data
+def buildTrain(train, pastDay=30, futureDay=5):
+  X_train, Y_train = [], []
+  for i in range(train.shape[0]-futureDay-pastDay):
+    X_train.append(np.array(train.iloc[i:i+pastDay]))
+    Y_train.append(np.array(train.iloc[i+pastDay:i+pastDay+futureDay]["Adj Close"]))
+  return np.array(X_train), np.array(Y_train)
+
+
+# In[7]:
+
+
+#資料亂序
+def shuffle(X,Y):
+  np.random.seed(10)
+  randomList = np.arange(X.shape[0])
+  np.random.shuffle(randomList)
+  return X[randomList], Y[randomList]
+
+
+# In[8]:
+
+
+# 將Training Data取一部份當作Validation Data
+def splitData(X,Y,rate):
+  X_train = X[int(X.shape[0]*rate):]
+  Y_train = Y[int(Y.shape[0]*rate):]
+  X_val = X[:int(X.shape[0]*rate)]
+  Y_val = Y[:int(Y.shape[0]*rate)]
+  return X_train, Y_train, X_val, Y_val
+
+
+# In[10]:
+
+
+# 將輸出合併
+
+
+# read SPY.csv
+train = readTrain()
+
+# Augment the features (year, month, date, day)
+train_Aug = augFeatures(train)
+
+# Normalization
+train_norm = normalize(train_Aug)
+
+# build Data, use last 30 days to predict next 5 days
+X_train, Y_train = buildTrain(train_norm, 30, 5)
+
+# shuffle the data, and random seed is 10
+X_train, Y_train = shuffle(X_train, Y_train)
+
+# split training data and validation data
+X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+
+# In[11]:
+
+
+# 模型建置
+
+
+# In[12]:
+
+
+# 一對一模型
+def buildOneToOneModel(shape):
+  model = Sequential()
+  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2],return_sequences=True))
+  # output shape: (1, 1)
+  model.add(TimeDistributed(Dense(1)))    # or use model.add(Dense(1))
+  model.compile(loss="mse", optimizer="adam")
+  model.summary()
+  return model
+
+
+# In[13]:
+
+
+# 將過去的天數pastDay設為1，預測的天數futureDay也設為1
+train = readTrain()
+train_Aug = augFeatures(train)
+train_norm = normalize(train_Aug)
+# change the last day and next day 
+X_train, Y_train = buildTrain(train_norm, 1, 1)
+X_train, Y_train = shuffle(X_train, Y_train)
+X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+# from 2 dimmension to 3 dimension
+Y_train = Y_train[:,np.newaxis]
+Y_val = Y_val[:,np.newaxis]
+
+model = buildOneToOneModel(X_train.shape)
+callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
+
+# In[14]:
+
+
+# 多對一模型
+def buildManyToOneModel(shape):
+  model = Sequential()
+  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2]))
+  # output shape: (1, 1)
+  model.add(Dense(1))
+  model.compile(loss="mse", optimizer="adam")
+  model.summary()
+  return model
+
+
+# In[15]:
+
+
+# 需要設定的有pastDay=30、future=1 ，且Y_train 的維度需為二維
+train = readTrain()
+train_Aug = augFeatures(train)
+train_norm = normalize(train_Aug)
+# change the last day and next day 
+X_train, Y_train = buildTrain(train_norm, 30, 1)
+X_train, Y_train = shuffle(X_train, Y_train)
+# because no return sequence, Y_train and Y_val shape must be 2 dimension
+X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+model = buildManyToOneModel(X_train.shape)
+callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
+
+# In[16]:
+
+
+# 一對多模型
+def buildOneToManyModel(shape):
+  model = Sequential()
+  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2]))
+  # output shape: (5, 1)
+  model.add(Dense(1))
+  model.add(RepeatVector(5))
+  model.compile(loss="mse", optimizer="adam")
+  model.summary()
+  return model
+
+
+# In[17]:
+
+
+# 將pastDay 設為1, futureDay 設為5
+train = readTrain()
+train_Aug = augFeatures(train)
+train_norm = normalize(train_Aug)
+# change the last day and next day 
+X_train, Y_train = buildTrain(train_norm, 1, 5)
+X_train, Y_train = shuffle(X_train, Y_train)
+X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+# from 2 dimmension to 3 dimension
+Y_train = Y_train[:,:,np.newaxis]
+Y_val = Y_val[:,:,np.newaxis]
+
+model = buildOneToManyModel(X_train.shape)
+callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
+
+# In[18]:
+
+
+# 多對多模型 (輸入與輸出相同長度)
+def buildManyToManyModel(shape):
+  model = Sequential()
+  model.add(LSTM(10, input_length=shape[1], input_dim=shape[2], return_sequences=True))
+  # output shape: (5, 1)
+  model.add(TimeDistributed(Dense(1)))
+  model.compile(loss="mse", optimizer="adam")
+  model.summary()
+  return model
+
+
+# In[19]:
+
+
+# 將pastDay 以及futureDay 設為相同長度5
+train = readTrain()
+train_Aug = augFeatures(train)
+train_norm = normalize(train_Aug)
+# change the last day and next day 
+X_train, Y_train = buildTrain(train_norm, 5, 5)
+X_train, Y_train = shuffle(X_train, Y_train)
+X_train, Y_train, X_val, Y_val = splitData(X_train, Y_train, 0.1)
+
+# from 2 dimmension to 3 dimension
+Y_train = Y_train[:,:,np.newaxis]
+Y_val = Y_val[:,:,np.newaxis]
+
+model = buildManyToManyModel(X_train.shape)
+callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+model.fit(X_train, Y_train, epochs=1000, batch_size=128, validation_data=(X_val, Y_val), callbacks=[callback])
+
